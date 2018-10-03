7720e004c1829733851f99775de2a6f2f86ecf1d
EmilyScripts <emily.rever@gmail.com>
2018-10-03 16:08:36 +0100

    Coppied over files from old version

---
+++ components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <p class="subtitle">Blazing fast, zero configuration web application bundler</p>
+    </div>
+  );
+};
+
+export default App;
