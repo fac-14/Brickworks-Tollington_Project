6bb8ca69fb86709b1cc2d62d978282629c2ea258
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-10-04 17:03:58 +0100

    configure home page and one mainPageComponent

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Nav from './mainPageComponents/index';
+
+class Home extends React.Component {
+    
+  render() {
+    return( 
+      <div>
+    <h2> this is home</h2> 
+    <Nav />
+    </div>
+  )
+    }
+    
+  
+}
+
+
+export default Home;
+
