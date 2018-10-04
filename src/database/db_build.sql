4381c46064e6918d73f30752658d9facc5be485c
MissArray <33687551+MissArray@users.noreply.github.com>
2018-10-04 18:11:33 +0100

    Added linking table and checked all tables are correctly created in pgcli

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,19 +1,27 @@
 BEGIN;
+
 DROP TABLE IF EXISTS events CASCADE;
-DROP TABLE IF EXISTS themes CASCADE;
+DROP TABLE IF EXISTS topics CASCADE;
+
+/*DROP TABLE IF EXISTS resource CASCADE;*/
 
 CREATE TABLE events (
-id SERIAL PRIMARY KEY,
+event_id SERIAL PRIMARY KEY,
 event_name TEXT NOT NULL,
 event_description TEXT NOT NULL,
 date_time TIMESTAMP,
 event_location TEXT NOT NULL
 );
 
-CREATE TABLE themes (
-id SERIAL PRIMARY KEY,
+CREATE TABLE topics (
+topic_id SERIAL PRIMARY KEY,
 theme TEXT NOT NULL,
 theme_description TEXT NOT NULL
+);
+
+CREATE TABLE link_events_topics (
+link_event_id INTEGER REFERENCES events(event_id) NOT NULL,
+link_topic_id INTEGER REFERENCES topics(topic_id) NOT NULL
 );
 
 /* Escape special characters by prefixing them with two single quotes*/
@@ -23,13 +31,18 @@ INSERT INTO events (event_name, event_description, date_time, event_location) VA
 ('Youth Action', 'Help youth get skills', '2018-10-11 16:30:00', 'Tollington House'),
 ('Get Green', 'Let''s plant a tree', '2018-02-01 14:00:00', 'Town Hall');
 
-INSERT INTO themes ('poverty', 'homelessness', 'youth', 'elderly', 'employment', 'environment') VALUES
+INSERT INTO topics (theme, theme_description) VALUES
 ('poverty', 'Poverty'),
 ('homelessness', 'Homelessness'),
 ('youth', 'Youth'),
 ('elderly', 'Elderly'),
 ('employment', 'Employment'),
 ('environment', 'Environment');
+
+INSERT INTO link_events_topics (link_event_id, link_topic_id) VALUES
+(1, 1),
+(2, 3),
+(3, 6);
 
 COMMIT;
 
