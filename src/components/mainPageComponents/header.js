192cf2408b38ec7f9738a16730ee48dccb33dda0
harryyy27 <harryyy27@gmail.com>
2018-10-05 16:25:32 +0100

    server fixed

--- src/components/mainPageComponents/header.js
+++ src/components/mainPageComponents/header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import menuImg from '../../../public/images/menu.svg';
 import './style.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Nav = () => (
   <div>
     <h2>this is header</h2>
-    <a href='/'>Home</a>
-    <a href='http://localhost:5000/api/test'>Events</a>
+    <Link to='/'>Home</Link>
+    <Link to='/events'>Events</Link>
     <img className="menuImg" src={ menuImg }/>
   </div>
 )
