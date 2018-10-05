17cc904ee84379d6827ef2bae601a80ca617c1fe
EmilyScripts <emily.rever@gmail.com>
2018-10-05 11:05:27 +0100

    add events page

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 // import { render } from 'react-dom';
 
 import Home from './pages/home';
+import Events from './pages/events';
+import Nav from './mainPageComponents/header';
 
 const App = () => {
   return (
     <div>
+      <Nav />
       <Home />
+      <Events />
     </div>
   );
 };
---
+++ src/components/pages/events.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Events extends React.Component {
+    
+  state = { data:null };
+  render() {
+    return ( 
+      <div>
+        <h2> this is events</h2> 
+      </div>
+    )
+  }
+}
+
+export default Events;
