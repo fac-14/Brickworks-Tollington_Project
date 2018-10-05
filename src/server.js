ca1e41b573f89160c66d55aea4a8a2491edeb33f
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-10-05 14:28:41 +0100

    configure remaining database files, some configuration of react server

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(path.join(__dirname, '..', 'dist')));
 
+app.get('/api/test', (req, res) => {
+  res.send('hello world');
+})
+// app.post('/api/createEvents', createEvents);
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started at port: ' + port);
