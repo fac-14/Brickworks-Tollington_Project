7613f73fd9212e7c948ca29a3b98575c7c3c9a0d
harryyy27 <37116061+harryyy27@users.noreply.github.com>
2018-10-16 11:29:16 +0100

    Update getAllEventsAirTable.js

--- src/queryHandlers/getAllEventsAirTable.js
+++ src/queryHandlers/getAllEventsAirTable.js
@@ -2,34 +2,6 @@ const base = require('../controllers/database/db_connection');
 
 console.log('getAllEventsAirTable');
 
-// exports.get = (request, response) => {
-//   let allRecords = [];
-//   // const getAllEventsAirTable = new Promise( (resolve, reject) => {
-//   base('social_action_events').select({
-//     fields: [
-//       'event_id', 'event_name', 'event_description', 'event_location', 'event_date_time', 'categories', 'Photo', 'fullname_event_organiser', 'email_event_organiser', 'telephone_event_organiser'
-//     ]   //photos left
-//   }).eachPage((records, fetchNextPage) => {
-//     allRecords = [...allRecords, ...records];
-//     // allRecords.forEach( (record) => console.log(record.fields));
-//     fetchNextPage();
-//   }, (err) => {
-//     if (err) {
-//       response.status(500).send("something Broke");
-//     }
-//     else {
-//       response.send(allRecords);
-//     }
-//   }
-
-//   );
-
-// };
-// select({
-//   // Selecting the first 3 records in Grid view:
-//   filterByFormula: 'NOT({welcome_email_sent})',
-//   fields: ['first_name', 'email'],
-// })
 exports.get = (request, response) => {
   let allRecords = [];
   // const getAllEventsAirTable = new Promise( (resolve, reject) => {
