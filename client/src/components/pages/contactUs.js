6bf34c983a47896a1281e4add4e523fd57e9cf49
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-10-16 14:06:09 +0100

    add fields to register an interest to start a social action

--- client/src/components/pages/contactUs.js
+++ client/src/components/pages/contactUs.js
@@ -4,22 +4,27 @@ class ContactUs extends React.Component {
   state = {
     name: '',
     email: '',
-    description: ''
+    phone: '',
+    description: '',
+    cntWithCommunityAdviser: false,
+    trainCommunityAdviser: false,
+    // startSocialAction: true
   }
   handleChange = event => {
-    //   const target = event.target;
-    //   const value = event.target.value;
-    //   //sets target's names value to value , target.name could be name or email or description
-    //  this.setState({ [target.name]: value });
-
-    this.setState({ [event.target.name]: event.target.value });
+    // this.setState({ [event.target.name]: event.target.value });
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [target.name]: value });
+ 
   };
 
   handleSubmit = event => {
     event.preventDefault();
     console.log('CONTACT US MA')
     console.log(this.state)
-    const data = JSON.stringify(this.state);
+    const data = JSON.stringify({
+      startSocialAction: this.state
+    });
     console.log('data', data)
     fetch('/api/contactUs', {
       method: 'POST',
@@ -43,8 +48,32 @@ class ContactUs extends React.Component {
         <input type="text" id="name" name="name" value={this.state.name} onChange={this.handleChange} />
         <label htmlFor="email">'Email':</label>
         <input type="text" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
-        <label htmlFor="description">Description:</label>
-        <input type="text" id="description" name="description" value={this.state.description} onChange={this.handleChange} />
+        <label htmlFor="phone">'Contact number':</label>
+        <input type="text" id="phone" name="phone" value={this.state.phone} onChange={this.handleChange} />
+        <label htmlFor="description">Description of social action:</label>
+        <textarea name="description" cols="40" rows="10" value={this.state.description} onChange={this.handleChange}></textarea>
+        {/* <input type="text" id="description" name="description" value={this.state.description} onChange={this.handleChange} /> */}
+        <h3>please check the options applicable to you</h3>
+        <label htmlFor="cntWithCommunityAdviser">I want to connect with a local Community Organiser</label>
+        <input
+          type="checkbox"
+          id="cntWithCommunityAdviser"
+          name="cntWithCommunityAdviser"
+          checked={this.state.cntWithCommunityAdviser}
+          onChange={this.handleChange}
+        />
+
+        <label htmlFor="trainCommunityAdviser">I am interested in Community Organisers training</label>
+        <input
+          type="checkbox"
+          id="trainCommunityAdviser"
+          name="trainCommunityAdviser"
+          checked={this.state.trainCommunityAdviser}
+          onChange={this.handleChange}
+        />
+
+
+
         <button type="submit">Submit</button>
       </form>
     );
