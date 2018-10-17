13d533b29b814a44d0b4bd91fbd4f1dfcfe890f8
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-10-17 13:18:23 +0100

    create-event form has check boxes

--- client/src/components/pages/create-event.js
+++ client/src/components/pages/create-event.js
@@ -1,26 +1,22 @@
 import React from 'react';
 class CreateEvent extends React.Component {
-    // constructor(props) {
-    //   super(props),
-    //   this.
     state = { 
       name: '',
       email: '',
       phone: '',
       eventTitle: '',
-      // eventCategory: '',
+      description: '',
       date: '',
       time: '',
       venue: '',
-      contact: '',
-      description: ''
+      hireSpace: false,
+      listEvent: false,
       }
-    //  };
 
   handleChange = event => {
-    let newState = {};
-    newState[event.target.id] = event.target.value;
-    this.setState(newState);
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [target.name]: value });
   }
 
   handleSubmit = event => {
@@ -50,24 +46,42 @@ class CreateEvent extends React.Component {
     return ( 
       <form>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" value={this.state.name} onChange= {this.handleChange}/>
+        <input type="text" id="name" name="name" value={this.state.name} onChange= {this.handleChange}/>
         <label htmlFor="email">E-mail</label>
-        <input type="text" id="email" value={this.state.email} onChange= {this.handleChange}/>
+        <input type="text" id="email" name="email" value={this.state.email} onChange= {this.handleChange}/>
         <label htmlFor="phone">phone</label>
-        <input type="text" id="phone" value={this.state.phone} onChange= {this.handleChange}/>
+        <input type="text" id="phone" name="phone" value={this.state.phone} onChange= {this.handleChange}/>
         <label htmlFor="eventTitle">Event Title</label>
-        <input type="text" id="eventTitle" value={this.state.eventTitle} onChange= {this.handleChange}/>
-        <label htmlFor="description">Event Details</label>
-        <input type="text" id="description" value={this.state.eventDetails} onChange= {this.handleChange}/>
+        <input type="text" id="eventTitle" name="eventTitle" value={this.state.eventTitle} onChange= {this.handleChange}/>
+        {/* <label htmlFor="description">Event Details</label>
+        <input type="text" id="description" name="description" value={this.state.description} onChange= {this.handleChange}/> */}
+        <textarea name="description" id="description" cols="40" rows="10" value={this.state.description} onChange={this.handleChange}></textarea>
 
-        {/* <label htmlFor="eventCategory">Event Category</label>
-        <input type="text" id="eventCategory" value={this.state.eventCategory} onChange= {this.handleChange}/> */}
         <label htmlFor="date">Date</label>
-        <input type="text" id="date" value={this.state.date} onChange= {this.handleChange}/>
+        <input type="text" id="date" name="date" value={this.state.date} onChange= {this.handleChange}/>
         <label htmlFor="time">Time</label>
-        <input type="text" id="time" value={this.state.time} onChange= {this.handleChange}/>
+        <input type="text" id="time" name="time" value={this.state.time} onChange= {this.handleChange}/>
         <label htmlFor="venue">Venue</label>
-        <input type="text" id="venue" value={this.state.venue} onChange= {this.handleChange}/>
+        <input type="text" id="venue" name="venue" value={this.state.venue} onChange= {this.handleChange}/>
+
+        <h3>Please check the options applicable to you</h3>
+        <label htmlFor="hireSpace">I want to hire a meeting space</label>
+        <input className='checkbox'
+          type="checkbox"
+          id="hireSpace"
+          name="hireSpace"
+          checked={this.state.hireSpace}
+          onChange={this.handleChange}
+        />
+
+        <label htmlFor="listEvent">I am interested to list this event </label>
+        <input className='checkbox'
+          type="checkbox"
+          id="listEvent"
+          name="listEvent"
+          checked={this.state.listEvent}
+          onChange={this.handleChange}
+        />
         
         <button type="submit" onClick={this.handleSubmit}>Submit</button>
       </form>
