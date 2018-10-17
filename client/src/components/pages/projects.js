43e9d6bcb766b3a2e634d85c47df451dfc68ae23
EmilyScripts <emily.rever@gmail.com>
2018-10-17 13:36:58 +0100

    neatened up margins and spacing, added image to projects page

--- client/src/components/pages/projects.js
+++ client/src/components/pages/projects.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import BikeImg from '../../../public/images/bike.jpg';
  
 
 const Projects = () => (
     <div className='wrapper'>
     <h1 data-testid="projects-page">Project</h1>
     <main>
-        <img src='../../../client/public/images/bike.jpg' alt='bike photo'></img>
+        <img id='bike-img' src={BikeImg} alt='bike photo'></img>
         <p className='hub-description-two'>Brickworks Social Action Hub is making a difference to the lives of local young people by helping them learn new skills and build relationships with local police through bicycle maintenance sessions. Local young people aged 12-16 years old are encouraged and supported to come to Brickworks for six sessions and during the two hours that they are with us they learn how to repair punctures, replace broken cables and align gears and brake pads. The bicycles are donated by the local police and the young people get to keep the bike on successful completion of the course.</p>
     </main>
     </div>
--- client/src/components/pages/topten.js
+++ client/src/components/pages/topten.js
@@ -8,28 +8,35 @@ const TopTen= () => (
         <main>
             <h2>Social actions takes time, energy and passion. Here's our 10 top tips to get you started:</h2>
             <p className='hub-description-two'>
-            <br></br>
             1. Identify a problem, challenge or issue in your neighbourhood and be specific
             <br></br>
+            <hr></hr>
             2. Do some research so that you have a good picture of the current situation, develop and plan
             <br></br>
+            <hr></hr>
             3. Get together with others to explore the situation, share your research and explore solutions
             <br></br>
+            <hr></hr>
             4. Build a wider, more diverse community of collaborators who support your aims
             <br></br>
+            <hr></hr>
             5. Collaborate with those who might not share your opinions or support your aims so that you can overcome objections and gain different perspectives
             <br></br>
+            <hr></hr>
             6. Promote your social action in your community and through the media; create a video
             <br></br>
+            <hr></hr>
             7. Take action!
             <br></br>
+            <hr></hr>
             8. Evaluate, reflect and record the impact of your action
             <br></br>
+            <hr></hr>
             9. Share the impact of your action in creative and imaginative ways to capture attention
             <br></br>
+            <hr></hr>
             10. Don't give up, have patience and be committed
             </p>
-            <br></br>
             <label htmlFor="all-events">
             <Link to='/contactUs'><h3 className='link'>Click here to register an interest to start a social action!</h3></Link>
             </label>
