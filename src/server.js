f5e4e60e8694721dc2f969b0ee7cc599f2d6fd2c
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-10-18 13:08:59 +0100

    added env check

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const express = require('express')
 // const controllers  = require('../src/controllers')
 
-const app = express()
+
 // all requests which begin with /api will go through to apiRoutes
 const port = process.env.PORT || 5000;
 // app.use(controllers)
