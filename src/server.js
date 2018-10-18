510db4d4f7f9196dcd8deace48509fc4fbb1c097
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-10-18 13:06:28 +0100

    added env check

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const app = require('./app')
+const path = require('path')
+const express = require('express')
+// const controllers  = require('../src/controllers')
 
+const app = express()
 // all requests which begin with /api will go through to apiRoutes
 const port = process.env.PORT || 5000;
+// app.use(controllers)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'parcel:build')))
+}
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
