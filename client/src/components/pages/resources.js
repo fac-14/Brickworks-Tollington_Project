8b8fc95f5141f37dcfc0630f54af3876ce649fe8
MissArray <33687551+MissArray@users.noreply.github.com>
2018-10-18 11:05:02 +0100

    Restored again list and headings accessibility

--- client/src/components/pages/resources.js
+++ client/src/components/pages/resources.js
@@ -4,46 +4,47 @@ const Resources = () => (
     <div className='wrapper'>
         <h1 data-testid="resources-page">Resources</h1> 
             <main>
-                <h2>Social Actions takes time, energy and passion. Here's our 10 Top Tips to get you started:</h2>
-                <p className='hub-description-two'>
-                1. Identify a problem, challenge or issue in your neighbourhood and be specific
+            <p className='strong-txt'>Engaging in social action takes time, energy and passion.  Here are our top 10 tips to get you started</p>
+            
+           <ol>
+               <li>
+               Identify a problem, challenge or issue in your neighbourhood and be specific about what you want to tackle.
+               </li>
+               <li>
+                Do some research so that you have a good picture of the current situation, then develop and plan how to go about tackling the problem.
+                </li>
+                <li>
+                Get together with others to share your research and explore the problem and potential solutions.
+                </li>
+                <li>
+                Build a wider, more diverse community of collaborators who support your aims.
+                </li>
+                <li>
+                Collaborate with those who might not share your opinions or support your aims so that you can overcome objections and gain different perspectives.           
+                </li>
+                <li>
+                Promote your social action in your community and through the media; create a video and post it on the Brickworks website.
+                </li>
+                <li>
+                Take action!
+                </li>
+                <li>
+                Evaluate, reflect and record the impact of your action on the community.
+                </li>
+                <li>
+                Share the impact of your action in creative and imaginative ways to capture attention.
+                </li>
+                <li>Don't give up, have patience and be committed.</li>
+           </ol>
                 <br></br>
                 <hr></hr>
-                2. Do some research so that you have a good picture of the current situation, develop and plan
-                <br></br>
-                <hr></hr>
-                3. Get together with others to explore the situation, share your research and explore solutions
-                <br></br>
-                <hr></hr>
-                4. Build a wider, more diverse community of collaborators who support your aims
-                <br></br>
-                <hr></hr>
-                5. Collaborate with those who might not share your opinions or support your aims so that you can overcome objections and gain different perspectives
-                <br></br>
-                <hr></hr>
-                6. Promote your social action in your community and through the media; create a video
-                <br></br>
-                <hr></hr>
-                7. Take action!
-                <br></br>
-                <hr></hr>
-                8. Evaluate, reflect and record the impact of your action
-                <br></br>
-                <hr></hr>
-                9. Share the impact of your action in creative and imaginative ways to capture attention
-                <br></br>
-                <hr></hr>
-                10. Don't give up, have patience and be committed
-                <br></br>
-                <hr></hr>
-                </p>
-                <h2>Helpful links for further information</h2>
+                <p className='strong-txt'>Helpful links for further information</p>
                 <div className='resource-links'>
-                    <a className='resource-link' href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/591797/A_description_of_social_action.pdf">A description of social action</a>
+                    <a className='resource-link' href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/591797/A_description_of_social_action.pdf">&#x25C7; &ensp;An example of social action</a>
                     <br></br>
-                    <a className='resource-link' href='https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/591799/Leadership_and_culture_change_to_enable_social_action.pdf'>Leadership and culture change</a>
+                    <a className='resource-link' href='https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/591799/Leadership_and_culture_change_to_enable_social_action.pdf'>&#x25C7; &ensp;Leadership and culture change</a>
                     <br></br>
-                    <a className='resource-link' href='https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/591805/Enabling_social_action_methodology.pdf'>Methodology</a>
+                    <a className='resource-link' href='https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/591805/Enabling_social_action_methodology.pdf'>&#x25C7; &ensp;Methodology</a>
                 </div>
             </main>
     </div>
