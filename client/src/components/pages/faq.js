0016ab4945c3fdf35a4952ed2f76041f605537bd
MissArray <33687551+MissArray@users.noreply.github.com>
2018-10-19 13:45:28 +0100

    Replaced privacy-policy URL; relates #89

--- client/src/components/pages/faq.js
+++ client/src/components/pages/faq.js
@@ -20,7 +20,7 @@ class Faq extends React.Component {
                      <li><p className='li-p-strong'>&#8226; &ensp; Will it cost me to hire a meeting space at Brickworks and am I limited to specific days or times?</p>
                 <span className='li-content'>As we are developing Brickworks as a Social Action Hub we want to encourage local people to use the facilities this community centre offers. Brickworks is open seven days a week and from 08:00 through to 21:00 most days. We intend to offer the meeting space for free. Get in touch with us at Brickworks to find out more.</span></li>       
                      <li><p className='li-p-strong'>&#8226; &ensp; How are my personal details that I am entering being used?</p>
-                <span className='li-content'>You can read the Brickworks Social Action Hub privacy policy</span><a href='../../../public/assets/privacy-policy-hcca.pdf'> here</a>.</li>
+                <span className='li-content'>You can read the Brickworks Social Action Hub privacy policy</span><a href='https://hanleycrouch.files.wordpress.com/2018/06/privacy-policy-hcca.pdf'> here</a>.</li>
             </ul>
         </main>
         </div>
